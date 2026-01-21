98217ff8680f987d59b36719968dff8e5082f0b5
Kyekyeku-Tech <safooppong121@gmail.com>
2026-01-21 01:52:55 +0000

    update

--- src/App.jsx
+++ src/App.jsx
@@ -77,7 +77,7 @@ export default function App() {
 
       <Routes>
         {/* ROOT */}
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<RootRedirect />} />
 
 
         {/* LOGIN */}
